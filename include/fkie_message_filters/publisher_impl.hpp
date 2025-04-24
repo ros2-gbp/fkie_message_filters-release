c16e2f3ecce4fa8158f8f04a51ed15c4d4382643
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2025-04-24 22:29:06 +0200

    Rebase from 'rpm/humble/fkie_message_filters'

--- include/fkie_message_filters/publisher_impl.hpp
+++ include/fkie_message_filters/publisher_impl.hpp
@@ -3,6 +3,7 @@
  * fkie_message_filters
  * Copyright © 2018-2025 Fraunhofer FKIE
  * Author: Timo Röhling
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,54 +26,59 @@
 
 #include "publisher.hpp"
 
+#include <rclcpp/create_publisher.hpp>
+
 namespace fkie_message_filters
 {
 
 FKIE_MF_BEGIN_ABI_NAMESPACE
 
-template<class M, template<typename> class Translate>
-Publisher<M, Translate>::Publisher() noexcept
+template<class M, template<typename, typename> class Translate, class A>
+Publisher<M, Translate, A>::Publisher() noexcept
 {
 }
 
-template<class M, template<typename> class Translate>
-Publisher<M, Translate>::~Publisher()
+template<class M, template<typename, typename> class Translate, class A>
+Publisher<M, Translate, A>::~Publisher()
 {
     shutdown_monitor();
 }
 
-template<class M, template<typename> class Translate>
-Publisher<M, Translate>::Publisher(rclcpp::Node::SharedPtr& node, const std::string& topic, const rclcpp::QoS& qos,
-                                   const rclcpp::PublisherOptions& options) noexcept
+template<class M, template<typename, typename> class Translate, class A>
+template<class NodeT>
+Publisher<M, Translate, A>::Publisher(NodeT&& node, const std::string& topic, const rclcpp::QoS& qos,
+                                      const rclcpp::PublisherOptionsWithAllocator<A>& options)
 {
-    advertise(node, topic, qos, options);
+    advertise<NodeT>(std::forward<NodeT&&>(node), topic, qos, options);
 }
 
-template<class M, template<typename> class Translate>
-bool Publisher<M, Translate>::is_active() const noexcept
+template<class M, template<typename, typename> class Translate, class A>
+bool Publisher<M, Translate, A>::is_active() const noexcept
 {
     return pub_ ? pub_->get_subscription_count() > 0 : false;
 }
 
-template<class M, template<typename> class Translate>
-std::string Publisher<M, Translate>::topic() const noexcept
+template<class M, template<typename, typename> class Translate, class A>
+std::string Publisher<M, Translate, A>::topic() const noexcept
 {
     return pub_ ? pub_->get_topic_name() : std::string();
 }
 
-template<class M, template<typename> class Translate>
-void Publisher<M, Translate>::advertise(rclcpp::Node::SharedPtr& node, const std::string& topic, const rclcpp::QoS& qos,
-                                        const rclcpp::PublisherOptions& options) noexcept
+template<class M, template<typename, typename> class Translate, class A>
+template<class NodeT>
+void Publisher<M, Translate, A>::advertise(NodeT&& node, const std::string& topic, const rclcpp::QoS& qos,
+                                           const rclcpp::PublisherOptionsWithAllocator<A>& options)
 {
-    pub_ = node->create_publisher<MessageType>(topic, qos, options);
-    start_monitor(node);
+    pub_ =
+        rclcpp::create_publisher<MessageType, A, PublisherROS, NodeT>(std::forward<NodeT&&>(node), topic, qos, options);
+    start_monitor<NodeT>(std::forward<NodeT&&>(node));
     update_subscriber_state();
 }
 
-template<class M, template<typename> class Translate>
-void Publisher<M, Translate>::receive(helpers::argument_t<typename Translate<M>::FilterType> m) noexcept
+template<class M, template<typename, typename> class Translate, class A>
+void Publisher<M, Translate, A>::receive(helpers::argument_t<typename Translate<M, A>::FilterType> m)
 {
-    Translate<M>::publish(*pub_, m);
+    Translate<M, A>::publish(*pub_, m);
 }
 
 FKIE_MF_END_ABI_NAMESPACE
