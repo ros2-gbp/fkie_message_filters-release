964fa24f68906ab448d96f71963f34dfbef2128b
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2025-04-24 22:30:10 +0200

    Rebase from 'debian/jazzy/fkie_message_filters'

--- include/fkie_message_filters/subscriber_impl.hpp
+++ include/fkie_message_filters/subscriber_impl.hpp
@@ -3,6 +3,7 @@
  * fkie_message_filters
  * Copyright © 2018-2025 Fraunhofer FKIE
  * Author: Timo Röhling
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,68 +26,75 @@
 
 #include "subscriber.hpp"
 
+#include <rclcpp/create_subscription.hpp>
+#include <rclcpp/node_interfaces/get_node_parameters_interface.hpp>
+#include <rclcpp/node_interfaces/get_node_topics_interface.hpp>
+
 namespace fkie_message_filters
 {
 
 FKIE_MF_BEGIN_ABI_NAMESPACE
 
-template<class M, template<typename> class Translate>
-Subscriber<M, Translate>::Subscriber() noexcept
+template<class M, template<typename, typename> class Translate, class A>
+Subscriber<M, Translate, A>::Subscriber() noexcept
 {
 }
 
-template<class M, template<typename> class Translate>
-Subscriber<M, Translate>::Subscriber(const rclcpp::Node::SharedPtr& node, const std::string& topic,
-                                     const rclcpp::QoS& qos, const rclcpp::SubscriptionOptions& options) noexcept
+template<class M, template<typename, typename> class Translate, class A>
+template<class NodeT>
+Subscriber<M, Translate, A>::Subscriber(NodeT&& node, const std::string& topic, const rclcpp::QoS& qos,
+                                        const rclcpp::SubscriptionOptionsWithAllocator<A>& options)
 {
-    subscribe(node, topic, qos, options);
+    subscribe<NodeT>(std::forward<NodeT&&>(node), topic, qos, options);
 }
 
-template<class M, template<typename> class Translate>
-std::string Subscriber<M, Translate>::topic() const noexcept
+template<class M, template<typename, typename> class Translate, class A>
+std::string Subscriber<M, Translate, A>::topic() const noexcept
 {
     return sub_ ? sub_->get_topic_name() : std::string();
 }
 
-template<class M, template<typename> class Translate>
-void Subscriber<M, Translate>::set_subscribe_options(const rclcpp::Node::SharedPtr& node, const std::string& topic,
-                                                     const rclcpp::QoS& qos,
-                                                     const rclcpp::SubscriptionOptions& options) noexcept
+template<class M, template<typename, typename> class Translate, class A>
+template<class NodeT>
+void Subscriber<M, Translate, A>::set_subscribe_options(NodeT&& node, const std::string& topic, const rclcpp::QoS& qos,
+                                                        const rclcpp::SubscriptionOptionsWithAllocator<A>& options)
 {
     unsubscribe();
-    node_ = node;
+    node_parameters_ = rclcpp::node_interfaces::get_node_parameters_interface(node);
+    node_topics_ = rclcpp::node_interfaces::get_node_topics_interface(node);
     topic_ = topic;
     qos_ = qos;
     options_ = options;
 }
 
-template<class M, template<typename> class Translate>
-void Subscriber<M, Translate>::subscribe(const rclcpp::Node::SharedPtr& node, const std::string& topic,
-                                         const rclcpp::QoS& qos, const rclcpp::SubscriptionOptions& options) noexcept
+template<class M, template<typename, typename> class Translate, class A>
+template<class NodeT>
+void Subscriber<M, Translate, A>::subscribe(NodeT&& node, const std::string& topic, const rclcpp::QoS& qos,
+                                            const rclcpp::SubscriptionOptionsWithAllocator<A>& options)
 {
-    set_subscribe_options(node, topic, qos, options);
+    set_subscribe_options<NodeT>(std::forward<NodeT&&>(node), topic, qos, options);
     subscribe();
 }
 
-template<class M, template<typename> class Translate>
-bool Subscriber<M, Translate>::is_configured() const noexcept
+template<class M, template<typename, typename> class Translate, class A>
+bool Subscriber<M, Translate, A>::is_configured() const noexcept
 {
-    return node_ && !topic_.empty();
+    return node_parameters_ && node_topics_ && !topic_.empty();
 }
 
-template<class M, template<typename> class Translate>
-void Subscriber<M, Translate>::subscribe_impl() noexcept
+template<class M, template<typename, typename> class Translate, class A>
+void Subscriber<M, Translate, A>::subscribe_impl() noexcept
 {
     if (!sub_)
     {
-        sub_ = node_->create_subscription<MessageType, SubscriptionCB>(
-            topic_, qos_, [this](SubscriptionType message) { this->send(Translate<M>::subscriberToFilter(message)); },
-            options_);
+        sub_ = rclcpp::create_subscription<MessageType, SubscriptionCB, A>(
+            node_parameters_, node_topics_, topic_, qos_,
+            [this](SubscriptionType message) { this->send(Translate<M, A>::subscriberToFilter(message)); }, options_);
     }
 }
 
-template<class M, template<typename> class Translate>
-void Subscriber<M, Translate>::unsubscribe_impl() noexcept
+template<class M, template<typename, typename> class Translate, class A>
+void Subscriber<M, Translate, A>::unsubscribe_impl() noexcept
 {
     sub_.reset();
 }
